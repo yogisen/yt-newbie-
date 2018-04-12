c5979240a92970437f41000cda997775b0bdcffd
yogisen <36984500+yogisen@users.noreply.github.com>
2018-04-12 09:53:19 +0200

    Add files via upload

--- Comparetableaux.java
+++ Comparetableaux.java
@@ -4,17 +4,34 @@ public class Comparetableaux {
 	public static void main(String[] args) {
 		
 		int[] tab1 = {1,2,3};
-		int[] tab2 = {1,2,3};
+		int[] tab2 = {1,5,3};
 		
 		if (tab1.length != tab2.length) {
-			System.out.println("Differents");
+			System.out.println("Differents1");
 		} else {
 			int i = 0;
 			while( i < tab1.length && tab1[i] == tab2[i]) {
 				++i;
-				System.out.println("same Board");
+				System.out.println("identifque1");
 			}
-			System.out.println("same Board test");
+			System.out.println("identique2");
+			
+			if ( i >= tab1.length) {
+				System.out.print("identique 3");
+				} else {
+					System.out.println("Differents2");
+
+				}
+			
+			
+/*
+ * identifque1
+identique2
+Differents2
+			
+ */
+			
+			
 
 			
 		}
