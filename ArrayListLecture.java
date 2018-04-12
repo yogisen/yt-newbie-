f488b65ea329c10a54cd222dc8764b159af613e2
yogisen <36984500+yogisen@users.noreply.github.com>
2018-04-12 11:13:00 +0200

    Add files via upload

---
+++ ArrayListLecture.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrayListLecture {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> tab = new ArrayList<String>();
+		System.out.print(tab.size());
+	}
+
+}
