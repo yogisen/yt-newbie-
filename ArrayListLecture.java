6d6d728b74376b66de0f0ae7cf4b4c2e1cf3aa2a
yogisen <36984500+yogisen@users.noreply.github.com>
2018-04-12 11:53:42 +0200

    Add files via upload

--- ArrayListLecture.java
+++ ArrayListLecture.java
@@ -4,8 +4,51 @@ public class ArrayListLecture {
 
 	public static void main(String[] args) {
 		
-		ArrayList<String> tab = new ArrayList<String>();
-		System.out.print(tab.size());
+		ArrayList<Integer> tab = new ArrayList<Integer>();
+		
+		// add ajoute une donnée
+		
+		tab.add(5);
+		System.out.println("valeur à l'emplacement 0 du tableau est : "+tab.get(0));
+		System.out.println("taille du tableau = "+tab.size());
+		
+		// set modifie une donnée
+		tab.set(0, 12);
+		
+		System.out.println("valeur à l'emplacement 0 du tableau est : "+tab.get(0));
+		
+		tab.add(5);
+		tab.add(105);
+		tab.add(52);
+		System.out.println("taille du tableau = "+tab.size());
+		
+		
+		//efface le tableau
+		//tab.clear();
+		// efface l'emplacement 0
+		tab.remove(0);
+		
+		if (tab.isEmpty()) {
+			System.out.println(" tableau vide  ");
+
+		}else {
+			
+		}		
+		System.out.println("taille du tableau = "+tab.size());
+		//System.out.println("valeur à l'emplacement 0 du tableau est : "+tab.get(0));
+
+
+		/*RESULTAT
+		 * valeur à l'emplacement 0 du tableau est : 5
+taille du tableau = 1
+valeur à l'emplacement 0 du tableau est : 12
+taille du tableau = 4
+taille du tableau = 3
+		 */
+
+
+
+		
 	}
 
 }
