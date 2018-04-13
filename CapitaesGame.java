46ca97ec262b4d1b36cc1d1f83a96e4fef3e6dc2
yogisen <36984500+yogisen@users.noreply.github.com>
2018-04-13 14:26:01 +0200

    Add files via upload

--- CapitaesGame.java
+++ CapitaesGame.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,11 +6,50 @@ public class CapitaesGame {
 
 	public static void main(String[] args) {
 		
-		final int NUMBER_OF_QUESTION = 7;
-		int index;
-		String pays, capitales;
+		final int NUMBER_OF_QUESTION = 2;
+		int score = 0, index;
+		String pays, capitales, user_answer;
 		
-		String [][] data = {
+		ArrayList<Integer> indexexAlreadyTaken = new ArrayList<>();
+		
+		String [][] data = getData();
+		
+		Scanner clavier = new Scanner(System.in);
+		
+		for (int i = 0; i < NUMBER_OF_QUESTION; i++) {
+			
+			// choisir de maniere aleatoire
+			
+			
+			do {
+				Random random = new Random();
+				index = random.nextInt(data.length);
+			}while(indexexAlreadyTaken.contains(index));
+			
+			indexexAlreadyTaken.add(index);
+			
+			
+			pays = data[index][0];
+			capitales = data[index][1];
+			System.out.println();
+			
+			System.out.printf("quelle est la capitale de ce pays : %s?\n" , pays);
+			user_answer = clavier.nextLine();
+			
+			if(capitales.equalsIgnoreCase(user_answer)) {
+				System.out.println("bonne reponse");
+				score++;
+			}else {
+					System.out.printf("Mauvaise reponse. il fallait repondre %s\n.", capitales);
+			}
+		}
+		System.out.printf("score est de %s\n",score);
+		System.out.printf("autre affichage<<Score : %d/%d>> ", score, NUMBER_OF_QUESTION);
+		clavier.close();
+	}
+	
+	static String [][] getData(){
+		 String [][] data = {
 				{"Senegale", "Dakar"},
 				{"France", "Paris"},
 				{"Nigeria", "Lagos"},
@@ -20,22 +60,7 @@ public class CapitaesGame {
 				{"Italie", "Rome"},
 				{"pays", "capitale"},
 		};
-		Scanner clavier = new Scanner(System.in);
-		
-		for (int i = 0; i < NUMBER_OF_QUESTION; i++) {
-			
-			// choisir de maniere aleatoire
-			
-			Random random = new Random();
-			
-			index = random.nextInt(data.length);
-			pays = data[index][0];
-			capitales = data[index][1];
-			System.out.println();
-			
-			System.out.printf("quelle est la capitale de ce pays : %s?\n" , pays);
-			
-		}
+		 return data;
 	}
 
 }
