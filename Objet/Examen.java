a430314fa7f71f856c8de907088efb999d5a0d42
yogisen <36984500+yogisen@users.noreply.github.com>
2018-04-19 13:43:16 +0200

    Add files via upload

---
+++ Objet/Examen.java
@@ -0,0 +1,9 @@
+package com.yogisen.javainteremdiaire;
+
+public class Examen {
+	
+	public void afficherMessage() {
+		System.out.println(" Bienvenue à cet Exam");
+	}
+
+}
---
+++ Objet/ExamenTest.java
@@ -0,0 +1,18 @@
+package com.yogisen.javainteremdiaire;
+
+public class ExamenTest {
+
+	public static void main(String[] args) {
+		
+		String test = "abc";
+	 String name = " je suis une chaine de caractere";
+	 String name1 = new String (" je suis une chaine de caractere 1");
+	 
+	 Examen examen = new Examen();
+	 examen.afficherMessage();
+
+	 System.out.println(name);
+	 System.out.println(name1);
+	}
+
+}
