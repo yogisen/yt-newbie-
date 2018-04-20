47ad69a7d92983c034bf178fc820fa8340322048
yogisen <36984500+yogisen@users.noreply.github.com>
2018-04-20 09:59:00 +0200

    Add files via upload

--- Objet/Task.java
+++ Objet/Task.java
@@ -4,15 +4,21 @@ public class Task {
 	
 	public String title;
 	public String description;
+	public int time;
 	public boolean completed = false;
+	public int recupere_tps;
 	
-	public Task(String titre, String desc) {
+	public Task(String titre, String desc, int temps) {
 		title = titre; 
 		description = desc;
+		time = temps;
 		
 	}
 	public void completed() {
 		completed = true;
+	
+	
+	
 	}
 
 }
--- Objet/Tasktest.java
+++ Objet/Tasktest.java
@@ -4,15 +4,20 @@ public class Tasktest {
 
 	public static void main(String[] args) {
 		
-		Task task1 = new Task("Titre1" , "task 1 ");
-		Task task2 = new Task(" titre 2", "task 2 ");
-		Task task3 = new Task(" callme", "avant 8 heures ");
+		Task task1 = new Task("Titre1" , "task 1 ", 60);
+		Task task2 = new Task(" titre 2", "task 2 ", 80);
+		Task task3 = new Task(" callme", "avant 8 heures ", 40);
 		task2.completed();
-	
+		
 		System.out.println(task1.title);
+		System.out.println(task1.time);
+		if (task1.time>40) {
+			System.out.println("so long ");
+			
+		}
 		System.out.println(task1.description);
 		System.out.println(task1.completed);
-		
+		/*
 		System.out.println(task2.title);
 		System.out.println(task2.description);
 		System.out.println(task2.completed);
@@ -21,7 +26,7 @@ public class Tasktest {
 		System.out.println(task3.description);
 		System.out.println(task3.completed);
 
-		
+		*/
 		
 	}
 
