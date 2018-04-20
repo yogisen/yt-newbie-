363af623dbca26364f8e1e74c57a40a6cc828070
yogisen <36984500+yogisen@users.noreply.github.com>
2018-04-20 10:49:04 +0200

    Add files via upload

--- Objet/ExamenTest.java
+++ Objet/ExamenTest.java
@@ -3,23 +3,40 @@ import java.util.Scanner;
 public class ExamenTest {
 
 	public static void main(String[] args) {
+		
+		int answer2 = 4;
+		// string answer 1
 				
 			Scanner keyboard = new Scanner(System.in);
 			 Examen examen = new Examen();
 			 examen.printMessage("programmation");
 			 examen.durationOfProof(120);
+			 
 			 System.out.println("Java is difficult program ? ");
 			 String answer1 = keyboard.nextLine();
+			 if (answer1 != "no") {
+				 System.out.println(" this answer was no"); 
+			 
 			 examen.question1(answer1);
-			 System.out.println(" 2*2 ");
-			 int answer2 = keyboard.nextInt();
-			 examen.question2(answer2);
+			 
+			do {
+				System.out.println(" how much 2 * 2 ");
+					 answer2 = keyboard.nextInt();
+				 if (answer2 !=4)
+					 System.out.println(" it's not possible please try again ");
+				 examen.question2(answer2);
+			} while  (answer2 != 4);
+		 
 			 examen.thx();
 			 examen.goodbye();
+			 
+			
+				
+			}
 						 
 	}
-
 }
+
 
 
 /* ce que j'aimerai faire
@@ -32,6 +49,3 @@ public class ExamenTest {
 		} while (condition);
 		
 		*/
-
-
-
